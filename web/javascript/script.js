aa406381d9c30783c687e6050a6cd998111678a8
micosara <micosara365@gmail.com>
2024-09-20 17:30:13 +0900

    9/20 web di

--- web/javascript/script.js
+++ web/javascript/script.js
@@ -3,3 +3,16 @@ $('.navi>li').mouseover(function(){
 }).mouseout(function(){
     $(this).find('.submenu').stop().slideUp(500);
 });
+
+$('.imgslide a:gt(0)').hide();
+setInterval(function(){
+  $('.imgslide a:first-child').fadeOut()
+     .next('a').fadeIn()
+     .end().appendTo('.imgslide');},3000);
+
+$('.tabmenu>li>a').click(function(){
+    $(this).parent().addClass("active").siblings().removeClass("active");
+    return false;
+});
+  
+          
