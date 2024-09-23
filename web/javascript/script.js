9f36665647cd161d0cc02c22e740b6a508c11207
micosara <micosara365@gmail.com>
2024-09-23 17:11:59 +0900

    9/23 web di complete

--- web/javascript/script.js
+++ web/javascript/script.js
@@ -13,6 +13,12 @@ setInterval(function(){
 $('.tabmenu>li>a').click(function(){
     $(this).parent().addClass("active").siblings().removeClass("active");
     return false;
-});
+});  
   
-          
+$(".notice li:first").click(function(){
+	$("#modal").addClass("active");
+});          
+
+$(".btn").click(function(){
+	$("#modal").removeClass("active");
+  });
