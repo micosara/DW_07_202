78166ffca07971b8c207eb922a52dc76f8842cdf
micosara <micosara365@gmail.com>
2024-10-23 17:33:55 +0900

    10/23 web-di ing..

--- web_di/01_src/js/script.js
+++ web_di/01_src/js/script.js
@@ -3,3 +3,13 @@ $('.navi>li').mouseover(function(){
 }).mouseout(function(){
     $(this).find('.submenu').stop().slideUp(500);
 });
+
+$('.imgslide a:gt(0)').hide();
+setInterval(function(){
+    $('.imgslide a:first-child')
+    .fadeOut(1000)
+    .next('a')
+    .fadeIn(1000)
+    .end()
+    .appendTo('.imgslide');
+},3000);  
